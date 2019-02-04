0eb3a17181c3d9e67268dbe337504f4ffa48598f
Archwizard <archwizard101@gmail.com>
2019-02-04 12:48:45 -0800

    Added nocolor support

--- zshprompt.zsh
+++ zshprompt.zsh
@@ -1,4 +1,5 @@
 #!/bin/zsh
+
 setopt prompt_subst
 function git_custom_status {
 	git status --short &> /dev/null
@@ -10,14 +11,30 @@ function git_custom_status {
 	else
 		DIFF=" x"
 fi
+        if [ $NOCOLORS!="TRUE"] 
+        then
 		echo "%{$fg[yellow]%}($(git status -b --short | sed 's/## //g' |awk -F. '{print $1}' | head -n 1)$DIFF)%{$reset_color%}"
+        else
+		echo "($(git status -b --short | sed 's/## //g' |awk -F. '{print $1}' | head -n 1)$DIFF)"
+fi
+
+        
 	fi
 }
 function zle-line-init zle-keymap-select {
+        if [ $NOCOLORS!="TRUE"] 
+        then
     return_code="%(?..%{$fg[red]%})"
     VIM_PROMPT="%{$fg[white]%} [% NORMAL]% %{$reset_color%}"
     RPS1="${${KEYMAP/vicmd/$VIM_PROMPT}/(main|viins)/} $(git_custom_status) $EPS1"
     PROMPT="%{$fg[blue]%}%m%{$fg[white]%}::%{$fg[cyan]%}%~%{$fg[green]%}${return_code} %#> %{$reset_color%}"
+else
+    return_code="%(?..)"
+    VIM_PROMPT="[% NORMAL]% "
+    RPS1="${${KEYMAP/vicmd/$VIM_PROMPT}/(main|viins)/} $(git_custom_status) $EPS1"
+    PROMPT="%m::%~${return_code} %#> "
+
+fi
     zle reset-prompt
 }
 
